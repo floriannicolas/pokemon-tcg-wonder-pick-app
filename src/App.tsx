fdc40092b4c2f12c610905b8aa41467892802ac7
ticlekiwi <florian.nicolas@yahoo.fr>
2025-01-24 23:04:07 +0100

    cleaning code

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useMemo, useState } from 'react'
 import './styles/animations.css'
 import './styles/App.css'
 import { getInitialCards } from './utils/initial-cards';
+import { delay } from './utils/delay';
 
 
 const FORCE_PIDGEY = false;
@@ -19,21 +20,17 @@ function App() {
   const [selectedCard, setSelectedCard] = useState<string | null>(null);
   const [gameState, setGameState] = useState('');
 
-  const launchWonderPick = () => {
+  const launchWonderPick = async () => {
     setGameState('flipped');
-    setTimeout(() => {
-      setCards(cards.sort(() => 0.5 - Math.random()));
-      setGameState('flipped centered');
-      setTimeout(() => {
-        setGameState('flipped centered shuffled');
-        setTimeout(() => {
-          setGameState('flipped');
-          setTimeout(() => {
-            setGameState('flipped selectable');
-          }, 800);
-        }, 1600);
-      }, 800);
-    }, 1800);
+    await delay(1800);
+    setCards(cards.sort(() => 0.5 - Math.random()));
+    setGameState('flipped centered');
+    await delay(800);
+    setGameState('flipped centered shuffled');
+    await delay(1600);
+    setGameState('flipped');
+    await delay(800);
+    setGameState('flipped selectable');
   }
 
   const resetGame = () => {
@@ -46,66 +43,67 @@ function App() {
     setGameState('');
   }
 
-  const selectCard = (index: number, url: string) => {
+  const selectCard = async (targetIndex: number, targetCard: string) => {
     if (selectedCard || !gameState.includes('selectable')) {
       return;
     }
 
-    const cardItem = document.getElementById(`card-${game}-${index}`);
-    if (cardItem) {
-      const starsContainer = document.createElement('div');
-      starsContainer.className = 'stars-container';
-
-      for (let i = 0; i < 6; i++) {
-        const star = document.createElement('div');
-        star.className = 'star';
-        star.style.left = Math.random() * 100 + '%';
-        star.style.top = Math.random() * 100 + '%';
-        star.style.animation = `star-twinkle 0.6s ease-out ${i * 0.1}s forwards`;
-        starsContainer.appendChild(star);
-      }
-
-      cardItem.appendChild(starsContainer);
-
-      const frontList = cardItem.getElementsByClassName('front');
-      if (frontList && frontList.length > 0) {
-        const front = frontList[0] as HTMLDivElement;
-        front.style.backgroundImage = `url(${forcedCard})`;
-      }
-      cardItem.className += ' picked new';
-      setSelectedCard(url);
-      setTimeout(() => {
-        cardItem.className = 'card picked'
-        setCards(cards.map((cardUrl, i) => {
-          if (i === index) {
-            return forcedCard;
-          }
-          if (cardUrl === forcedCard && url !== forcedCard) {
-            return url;
-          }
-          return cardUrl;
-        }));
-        setTimeout(() => {
-          setGameState('end');
-        }, 300);
-      }, 1400);
+    const cardItem = document.getElementById(`card-${game}-${targetIndex}`);
+    if (!cardItem) {
+      return;
     }
+    const starsContainer = document.createElement('div');
+    starsContainer.className = 'stars-container';
+
+    for (let i = 0; i < 6; i++) {
+      const star = document.createElement('div');
+      star.className = 'star';
+      star.style.left = Math.random() * 100 + '%';
+      star.style.top = Math.random() * 100 + '%';
+      star.style.animation = `star-twinkle 0.6s ease-out ${i * 0.1}s forwards`;
+      starsContainer.appendChild(star);
+    }
+
+    cardItem.appendChild(starsContainer);
+
+    const frontList = cardItem.getElementsByClassName('front');
+    if (frontList && frontList.length > 0) {
+      const front = frontList[0] as HTMLDivElement;
+      front.style.backgroundImage = `url(${forcedCard})`;
+    }
+    cardItem.className = 'card picked new';
+    setSelectedCard(targetCard);
+    await delay(1400);
+
+    cardItem.className = 'card picked'
+    setCards(cards.map((card, index) => {
+      if (index === targetIndex) {
+        return forcedCard;
+      }
+      if (card === forcedCard && targetCard !== forcedCard) {
+        return targetCard;
+      }
+      return card;
+    }));
+
+    await delay(300);
+    setGameState('end');
   }
 
   return (
     <>
       <div className={`content-cards ${gameState}`}>
-        {cards.map((url, index) => (
+        {cards.map((card, index) => (
           <div
             id={`card-${game}-${index}`}
             className='card'
             key={`card-${game}-${index}`}
             onClick={() => {
-              selectCard(index, url);
+              selectCard(index, card);
             }}
           >
             <div className="card-inner">
-              <div className="front" style={{ backgroundImage: 'url(' + url + ')' }} />
+              <div className="front" style={{ backgroundImage: `url(${card})` }} />
               <div className="back" style={{ backgroundImage: 'url(/cards/back.webp)' }} />
             </div>
           </div>

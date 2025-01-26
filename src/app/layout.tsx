f68179b4865a7ceeff08d819047164575bb96735
ticlekiwi <florian.nicolas@yahoo.fr>
2025-01-26 18:07:07 +0100

    Adding vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles/globals.css";
 import GithubRibbon from "@/components/github-ribbon";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <GithubRibbon />
+        <Analytics />
         {children}
       </body>
     </html>

1a09e6b86b35f755162f8b138023f4c123ab51f9
ticlekiwi <florian.nicolas@yahoo.fr>
2025-01-28 13:30:48 +0100

    updating action buttons style

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -192,13 +192,13 @@ export default function Game({
                         <>
                             <button
                                 onClick={startWonderPick}
-                                className="px-10 py-2.5 h-12 box-border font-medium border-none bg-gradient-to-t from-[#37e6d5] to-[#1decb5] text-white rounded-full transition-all duration-300 ease-in-out shadow-md select-none text-base cursor-pointer hover:from-[#51F7CB] hover:to-[#1decb5] focus:outline-none"
+                                className="px-10 py-2.5 h-12 box-border font-medium border-none bg-gradient-to-t from-[#37e6d5] to-[#1decb5] text-white rounded-full transition-all duration-300 ease-in-out shadow-md select-none text-base cursor-pointer hover:from-[#51F7CB] hover:to-[#1decb5] active:shadow-xl"
                             >
                                 Start
                             </button>
                             <button
                                 onClick={() => { resetGame(); }}
-                                className="relative px-10 py-2.5 h-12 box-border font-medium border-none bg-gradient-to-t from-[#fff] to-[#fff] rounded-full transition-all duration-300 ease-in-out select-none shadow-md text-[#3A4452] cursor-pointer hover:from-[#f4f4f4] hover:to-[#f4f4f4] focus:outline-none"
+                                className="relative px-10 py-2.5 h-12 box-border font-medium border-none bg-gradient-to-t from-[#fff] to-[#fff] rounded-full transition-all duration-300 ease-in-out select-none shadow-md text-[#3A4452] cursor-pointer hover:from-[#f4f4f4] hover:to-[#f4f4f4] active:shadow-xl"
                             >
                                 <span className="absolute top-[3px] bottom-[3px] left-[3px] right-[3px] border-2 rounded-full border-[#55DBE8]" />
                                 Reload
@@ -208,7 +208,7 @@ export default function Game({
                     {gameState === 'end' && (
                         <button
                             onClick={() => { resetGame(); }}
-                            className="relative px-10 py-2.5 h-12 box-border font-medium border-none bg-gradient-to-t from-[#fff] to-[#fff] rounded-full transition-all duration-300 ease-in-out select-none shadow-md text-[#3A4452] cursor-pointer hover:from-[#f4f4f4] hover:to-[#f4f4f4] focus:outline-none"
+                            className="relative px-10 py-2.5 h-12 box-border font-medium border-none bg-gradient-to-t from-[#fff] to-[#fff] rounded-full transition-all duration-300 ease-in-out select-none shadow-md text-[#3A4452] cursor-pointer hover:from-[#f4f4f4] hover:to-[#f4f4f4] active:shadow-xl"
                         >
                             <span className="absolute top-[3px] bottom-[3px] left-[3px] right-[3px] border-2 rounded-full border-[#55DBE8]" />
                             New Pick
